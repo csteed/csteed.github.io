3a166736d1af6649d31188cc0d5f9cef47da8479
Chad A. Steed <steedca@ornl.gov>
2020-07-07 10:37:14 -0400

    New jekyll based site

--- art/drawings/make-thumbnails.sh
+++
@@ -1,6 +0,0 @@
-for i in `seq 1 28`
-do
-  origfname="d$i.jpg"
-  thumbfname="d$i-thumb.jpg"
-  convert $origfname -resize 250 $thumbfname
-done
--- art/pcpart/images/make-thumbnails.sh
+++
@@ -1,6 +0,0 @@
-for i in `seq 1 10`
-do
-  origfname="PCPArt$i.pdf"
-  thumbfname="PCPArt$i-thumb.png"
-  convert $origfname -resize 350 $thumbfname
-done
